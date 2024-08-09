28a1f1435df9d43a0a9bdfc97f84fc46e3f09f39
Savar Bhasin <51899822+savarbhasin@users.noreply.github.com>
2024-08-10 03:40:22 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -4,7 +4,7 @@ sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 import io
-from PyPDF2 import PdfReader
+from langchain_community.document_loaders import PyPDFLoader
 from dotenv import load_dotenv
 import google.generativeai as genai
 from PIL import Image
@@ -12,6 +12,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_chroma import Chroma
+from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
@@ -24,7 +25,7 @@ os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
 os.environ['OPENAI_API_KEY'] = os.getenv('OPENAI_API_KEY')
 
 model = ChatGroq(model="llama3-70b-8192")
-model = ChatOpenAI(model='gpt-4o-mini')
+# model = ChatOpenAI(model='gpt-4o-mini')
 
 prompt = ChatPromptTemplate.from_template("""
     You are an invoice reader AI.
@@ -45,7 +46,10 @@ st.title("Invoice Reader AI")
 
 uploaded_file = st.file_uploader("Upload file", type=["pdf", "png", "jpg", "jpeg"])
 
+
+
 if uploaded_file is not None:
+    
     file_type = uploaded_file.type.split('/')[1]
     splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=75)
     
@@ -54,39 +58,40 @@ if uploaded_file is not None:
         with st.spinner("Processing"):
             response = vision_model.generate_content(["Extract all text from the image", image, "You are an invoice reader."])
             documents = splitter.split_text(response.text)
-            vector_store = Chroma.from_texts(documents, embedding=embeddings)
+            vector_store = FAISS.from_texts(documents, embedding=embeddings)
+            
 
     if file_type == 'pdf':
         with st.spinner("Processing"):
-            pdf_file = io.BytesIO(uploaded_file.getvalue())
-            pdf_reader = PdfReader(pdf_file)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-            documents = splitter.split_text(text)
-            vector_store = Chroma.from_texts(documents, embedding=embeddings)
-    retriever = vector_store.as_retriever()
+            temppdf = f"./temp.pdf"
+            with open(temppdf, "wb") as f:
+                f.write(uploaded_file.getvalue())
+                file_name = uploaded_file.name
+
+            docs = PyPDFLoader(file_path=temppdf).load()
+            documents = splitter.split_documents(docs)
+            vector_store = FAISS.from_documents(documents, embedding=embeddings)
+            
+
+    retriever = vector_store.as_retriever()    
     
     first_chain = create_stuff_documents_chain(model, prompt, output_parser=JsonOutputParser())
-    retrieval_chain = create_retrieval_chain(retriever, first_chain)
-             
-        
-    
+    rag_chain = create_retrieval_chain(retriever, first_chain)
+
     with st.spinner("Fetching invoice details"):
-        answer = retrieval_chain.invoke({"input": "who is the invoice billed to (customer) give full details? what are the items? what is the total amount of the invoice?"})['answer']
+        answer = rag_chain.invoke({"input": "who is the invoice billed to (customer) give full details? what are the items? what is the total amount of the invoice?"})['answer']
         st.json(answer)
-    
+
     st.title("Chat with the document")
     question = st.text_input("Ask a question about the invoice:")
-    if st.button("Submit Question"):
-        if question:
-            with st.spinner("Generating response"):
-                answer = retrieval_chain.invoke({"input": question})['answer']
-                st.json(answer)
-        else:
-            st.warning("Please enter a question")
-        
-   
+    if question:
+        with st.spinner("Generating response"):
+            answer = rag_chain.invoke({"input": question})['answer']
+            st.json(answer)
 else:
     st.info("Please upload a PDF or image file to get started.")
         
+
+    
+   
+
